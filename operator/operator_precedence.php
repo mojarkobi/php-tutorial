b1d7e2d27531ec1ef517e3f10d25aa8a5f0c1d72
musicalashik <musicalashik24@gmail.com>
2023-11-26 09:57:31 +0600

    new file:   operator/operator_precedence.php 	new file:   string/concat string at the index number.php 	new file:   string/string concat all techniques.php

---
+++ operator/operator_precedence.php
@@ -0,0 +1,12 @@
+<?php
+ 
+
+   $n = 1 - 2 - 3;  // akhane ati same - diye kora r same precedence howate ati (1-2) - 3 arokom vabe evaluate hoyeche
+   
+
+   echo $n ."\n";
+
+ //  echo $m;
+
+
+   ?>
